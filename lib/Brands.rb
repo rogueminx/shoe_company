59259395eee2fe60724013ffbc0c1fabb5969433
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-02 13:48:12 -0800

    passes specs, validates presence, displays two decimal points, capitalizes.

--- lib/Brands.rb
+++ lib/Brands.rb
@@ -1,7 +1,13 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
-
+  validates(:brand_name, {:presence => true, :length => {maximum: 100}})
+  validates(:price, {:presence => true})
+  before_save(:normalize)
   private
 
-  
+  def normalize
+    self.brand_name = brand_name.downcase.titleize
+    self.price = '%.2f' % self.price
+  end
+
 end
--- spec/Brands_spec.rb
+++ spec/Brands_spec.rb
@@ -13,16 +13,16 @@ describe('Brand') do
 
   it("validates length of brand name is less than 100 characters") do
     brand = Brand.new({:brand_name => "a".*(101)})
-    expect(store.save()).to(eq(false))
+    expect(brand.save()).to(eq(false))
   end
 
   it("capitalizes the brand name") do
-    brand = Brand.create({:store_name => "some shoes"})
+    brand = Brand.create({:brand_name => "some shoes", :price => "50"})
     expect(brand.brand_name()).to(eq("Some Shoes"))
   end
 
   it("corrects the brand price") do
-    brand = Brand.create({:price => "50"})
-    expect(brand.price()).to(eq("50.00"))
+    brand = Brand.create({:brand_name => "some shoes", :price => 50})
+    expect(brand.price).to(eq(50.00))
   end
 end
