589566485ae99960445342c56511a8741091f85a
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-02 11:13:51 -0800

    delete and update stores.

--- app.rb
+++ app.rb
@@ -24,23 +24,36 @@ get('/stores') do
 end
 
 get('/store/:id') do
-  @store = Store.find(params.fetch("id"))
+  @store = Store.find(params['id'])
   erb:storeinfo
 end
 
 get('/brand/:id') do
-  @brand = Brand.find(params.fetch("id"))
+  @brand = Brand.find(params['id'])
   erb:brandinfo
 end
 
 post('/stores') do
-  store = Store.create({:store_name => params.fetch("store_name"), :id => nil})
+  store = Store.create({:store_name => params['store_name'], :id => nil})
   @stores = Store.all
   erb:stores
 end
 
 post('/brands') do
-  brand = Brand.create({:brand_name => params.fetch("brand_name"), :price => params.fetch("price"), :id => nil})
+  brand = Brand.create({:brand_name => params['brand_name'], :price => params['price'], :id => nil})
   @brands = Brand.all
   erb:brands
+end
+
+patch('/store/:id') do
+  @store = Store.find(params['id'])
+  @store.update({:store_name => params['store_name']})
+  erb:storeinfo
+end
+
+delete('/store/:id') do
+  store = Store.find(params['id'])
+  store.delete
+  @stores = Store.all
+  erb:stores
 end
