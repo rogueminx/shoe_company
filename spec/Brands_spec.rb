5efff9e58afc9ca09b3442734b385e5f117ffe36
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-02 13:07:26 -0800

    add specs for brand validation and format.

--- spec/Brands_spec.rb
+++ spec/Brands_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe('Brand') do
+  it("validates presence of brand name") do
+    brand = Brand.new({:brand_name => ""})
+    expect(brand.save()).to(eq(false))
+  end
 
+  it("validates presence of brand price") do
+    brand = Brand.new({:price => ""})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it("validates length of brand name is less than 100 characters") do
+    brand = Brand.new({:brand_name => "a".*(101)})
+    expect(store.save()).to(eq(false))
+  end
+
+  it("capitalizes the brand name") do
+    brand = Brand.create({:store_name => "some shoes"})
+    expect(brand.brand_name()).to(eq("Some Shoes"))
+  end
+
+  it("corrects the brand price") do
+    brand = Brand.create({:price => "50"})
+    expect(brand.price()).to(eq("50.00"))
+  end
 end
